88f5904a3082d5f35f604daff460a655455fac7f
Morten Jacobsen <mortenja@ubuntu>
2010-09-23 14:12:55 +0200

    Added a static method that creates a TimeInvariant object based on an input string, e.g. "<= 4" or "<inf"

--- src/TAPN/TimeInvariant.cpp
+++ src/TAPN/TimeInvariant.cpp
@@ -1,7 +1,32 @@
 #include "TimeInvariant.hpp"
+#include "boost/algorithm/string.hpp"
+#include "boost/lexical_cast.hpp"
 
 namespace VerifyTAPN {
 	namespace TAPN {
+		TimeInvariant TimeInvariant::createFor(const std::string& invariant)
+		{
+			bool strict = !boost::algorithm::icontains(invariant, "<=");
+			int bound = std::numeric_limits<int>().max();
 
+			int pos = strict ? 1 : 2;
+			std::string number = invariant.substr(pos);
+			boost::algorithm::trim(number);
+
+			if(!boost::algorithm::icontains(invariant, "inf"))
+			{
+				bound = boost::lexical_cast<int>(number);
+			}
+
+			return TimeInvariant(strict, bound);
+		}
+
+		void TimeInvariant::Print(std::ostream& out) const
+		{
+			std::string comparison = strictComparison ? "<" : "<=";
+			std::string strBound = bound == std::numeric_limits<int>().max() ? "inf" : boost::lexical_cast<std::string>(bound);
+
+			out << comparison << " " << strBound;
+		}
 	}
 }
--- src/TAPN/TimeInvariant.hpp
+++ src/TAPN/TimeInvariant.hpp
@@ -1,6 +1,7 @@
 #ifndef VERIFYTAPN_TAPN_TIMEINVARIANT_HPP_
 #define VERIFYTAPN_TAPN_TIMEINVARIANT_HPP_
 
+#include <string>
 #include <limits>
 
 namespace VerifyTAPN {
@@ -11,14 +12,22 @@ namespace VerifyTAPN {
 		public: // construction/destruction
 			TimeInvariant() : strictComparison(true), bound(std::numeric_limits<int>::max()) { };
 			TimeInvariant(bool strictComparison, int bound) : strictComparison(strictComparison), bound(bound) { };
+			TimeInvariant(const TimeInvariant& ti) : strictComparison(ti.strictComparison), bound(ti.bound) { };
+			TimeInvariant& operator=(const TimeInvariant& ti)
+			{
+				strictComparison = ti.strictComparison;
+				bound = ti.bound;
+				return *this;
+			}
 
 			virtual ~TimeInvariant() { /* Empty */ };
 		public: // inspectors
-
+			static TimeInvariant createFor(const std::string& invariant);
+			void Print(std::ostream& out) const;
 
 		private: // data
-			const bool strictComparison;
-			const int bound;
+			bool strictComparison;
+			int bound;
 		};
 
 	}
