047bd94d73a25972273b38c1a1f8071960bf32a9
Mathias Grund Sørensen <mgso09@student.aau.dk>
2012-03-05 15:24:41 +0100

    Less spam, more ham

--- src/DiscreteVerification/PWList.cpp
+++ src/DiscreteVerification/PWList.cpp
@@ -21,9 +21,6 @@ void PWList::Add(const NonStrictMarking& marking){
 	if(!markings_storage[marking.HashKey()].equals(marking)){
 		markings_storage[marking.HashKey()] = marking;
 		waiting_list.Add(marking);
-	}else{
-		std::cout << marking.HashKey() << " exists in PWList and is " << markings_storage[marking.HashKey()] << std::endl;
-		std::cout << "Marking is " << marking.size() << " fetch is " << markings_storage[marking.HashKey()].size() << std::endl;
 	}
 }
 
