45515181a663511952d44f205c64124bc84fd0d2
Jiri Srba <srba@cs.aau.dk>
2012-06-27 09:13:19 +0200

    changed verifytapn to verifydtapn in compilation scripts

--- src/compile-linux-32-64.sh
+++ src/compile-linux-32-64.sh
@@ -17,13 +17,13 @@ flex -o $parser/Generated/lexer.cpp $parser/flex.ll
 bison -o $parser/Generated/parser.cpp $parser/grammar.yy
 
 g++-4.4  -DBOOST_DISABLE_THREADS -DNDEBUG -DDISABLE_ASSERTX -static -O3 -Wall -mtune=core2 \
-    $src $inc64 $lib64 -o verifytapn64 && \
-strip verifytapn64 && \
+    $src $inc64 $lib64 -o verifydtapn64 && \
+strip verifydtapn64 && \
 echo "64-bit linux OK" &
 
 g++-4.4  -DBOOST_DISABLE_THREADS -DNDEBUG -DDISABLE_ASSERTX -static -O3 -Wall -mtune=core2 -m32 \
-    $src $inc32 $lib32 -o verifytapn32 && \
-strip verifytapn32 && \
+    $src $inc32 $lib32 -o verifydtapn32 && \
+strip verifydtapn32 && \
 echo "32-bit linux OK" &
 
 wait
--- src/compile-win-32.sh
+++ src/compile-win-32.sh
@@ -11,7 +11,7 @@ src=`find . -name "*.cpp"`
 bison -o $parser/Generated/parser.cpp $parser/grammar.yy
 
 i586-mingw32msvc-g++ -DBOOST_DISABLE_THREADS -DNDEBUG -DDISABLE_ASSERTX -static -O3 -Wall \
-    $src $incw32 $libw32 -o verifytapn32.exe && \
-i586-mingw32msvc-strip verifytapn32.exe && \
+    $src $incw32 $libw32 -o verifydtapn32.exe && \
+i586-mingw32msvc-strip verifydtapn32.exe && \
 echo "32-bit win OK" 
 
