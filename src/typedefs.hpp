1e817048b7a2b75a9661c96d8f58b7f871f1339f
Mathias Grund Sørensen <mgso09@student.aau.dk>
2012-07-02 08:56:25 +0200

    All unnecessary files removed

--- src/typedefs.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TYPEDEFS_HPP_
-#define TYPEDEFS_HPP_
-
-#include "boost/bimap.hpp"
-
-typedef boost::bimap<unsigned int, unsigned int> BiMap;
-typedef double decimal;
-typedef long long id_type;
-#endif /* TYPEDEFS_HPP_ */
