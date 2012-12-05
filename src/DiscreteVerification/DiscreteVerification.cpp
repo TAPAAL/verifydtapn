46b6a97e7e390c9b3d5fe9fe28b26c1cb56262f2
Peter Gjøl Jensen <peter.gjoel@gmail.com>
2012-12-05 10:48:41 +0100

    Added generic helpter function to handle verification output

--- src/DiscreteVerification/DiscreteVerification.cpp
+++ src/DiscreteVerification/DiscreteVerification.cpp
@@ -9,87 +9,101 @@
 
 namespace VerifyTAPN {
 
-namespace DiscreteVerification {
+    namespace DiscreteVerification {
 
-DiscreteVerification::DiscreteVerification() {
-	// TODO Auto-generated constructor stub
+        template<typename T> void VerifyAndPrint(Verification<T>* verifier, VerificationOptions& options, AST::Query* query);
+        
+        DiscreteVerification::DiscreteVerification() {
+            // TODO Auto-generated constructor stub
 
-}
+        }
 
-DiscreteVerification::~DiscreteVerification() {
-	// TODO Auto-generated destructor stub
-}
+        DiscreteVerification::~DiscreteVerification() {
+            // TODO Auto-generated destructor stub
+        }
 
-int DiscreteVerification::run(boost::shared_ptr<TAPN::TimedArcPetriNet>& tapn, std::vector<int> initialPlacement, AST::Query* query, VerificationOptions& options){
-	if(!(*tapn).IsNonStrict()){
-		std::cout << "The supplied net contains strict intervals." << std::endl;
-		return -1;
-	}
+        int DiscreteVerification::run(boost::shared_ptr<TAPN::TimedArcPetriNet>& tapn, std::vector<int> initialPlacement, AST::Query* query, VerificationOptions& options) {
+            if (!(*tapn).IsNonStrict()) {
+                std::cout << "The supplied net contains strict intervals." << std::endl;
+                return -1;
+            }
 
-	NonStrictMarking* initialMarking = new NonStrictMarking(*tapn, initialPlacement);
+            NonStrictMarking* initialMarking = new NonStrictMarking(*tapn, initialPlacement);
 
-	std::cout << "MC: " << tapn->MaxConstant() << std::endl;
+            std::cout << "MC: " << tapn->MaxConstant() << std::endl;
 #if DEBUG
-	std::cout << "Places: " << std::endl;
-	for(TAPN::TimedPlace::Vector::const_iterator iter = tapn.get()->GetPlaces().begin(); iter != tapn.get()->GetPlaces().end(); iter++){
-		std::cout << "Place " << iter->get()->GetIndex() << " has category " << iter->get()->GetType() << std::endl;
-	}
+            std::cout << "Places: " << std::endl;
+            for (TAPN::TimedPlace::Vector::const_iterator iter = tapn.get()->GetPlaces().begin(); iter != tapn.get()->GetPlaces().end(); iter++) {
+                std::cout << "Place " << iter->get()->GetIndex() << " has category " << iter->get()->GetType() << std::endl;
+            }
 #endif
 
-	if(initialMarking->size() > options.GetKBound())
-	{
-		std::cout << "The specified k-bound is less than the number of tokens in the initial markings.";
-		return 1;
-	}
+            if (initialMarking->size() > options.GetKBound()) {
+                std::cout << "The specified k-bound is less than the number of tokens in the initial markings.";
+                return 1;
+            }
 
-	std::cout << options << std::endl;
-	Verification<NonStrictMarking>* verifier = NULL;
+            std::cout << options << std::endl;
 
-	WaitingList<NonStrictMarking>* strategy = NULL;
-	// Select verification method
-	if(options.GetVerificationType() == DISCRETE){
-		WaitingList<NonStrictMarking>* strategy = GetWaitingList<NonStrictMarking>(query, options);
+            WaitingList<NonStrictMarking>* strategy = NULL;
+            // Select verification method
+            if (options.GetVerificationType() == DISCRETE) {
+                WaitingList<NonStrictMarking>* strategy = GetWaitingList<NonStrictMarking > (query, options);
 
-		if(query->GetQuantifier() == EG || query->GetQuantifier() == AF){
-			verifier = new LivenessSearch(tapn, *initialMarking, query, options, strategy);
-		}else if(query->GetQuantifier() == EF || query->GetQuantifier() == AG){
-			verifier = new ReachabilitySearch(tapn, *initialMarking, query, options, strategy);
-		}
-	}else if(options.GetVerificationType() == TIMEDART){
-		if(query->GetQuantifier() == EG || query->GetQuantifier() == AF){
-			WaitingList<WaitingDart>* strategy = GetWaitingList<WaitingDart>(query, options);
-			verifier = new TimeDartLiveness(tapn, *initialMarking, query, options, strategy);
-		}else if(query->GetQuantifier() == EF || query->GetQuantifier() == AG){
-			WaitingList<TimeDart>* strategy = GetWaitingList<TimeDart>(query, options);
-			verifier = new TimeDartReachabilitySearch(tapn, *initialMarking, query, options, strategy);
-		}
-	}
+                if (query->GetQuantifier() == EG || query->GetQuantifier() == AF) {
+                    VerifyAndPrint(
+                            new LivenessSearch(tapn, *initialMarking, query, options, strategy),
+                            options,
+                            query);
+                } else if (query->GetQuantifier() == EF || query->GetQuantifier() == AG) {
+                    VerifyAndPrint(
+                            new ReachabilitySearch(tapn, *initialMarking, query, options, strategy),
+                            options,
+                            query);
+                }
+            } else if (options.GetVerificationType() == TIMEDART) {
+                if (query->GetQuantifier() == EG || query->GetQuantifier() == AF) {
+                    WaitingList<WaitingDart>* strategy = GetWaitingList<WaitingDart > (query, options);
+                    VerifyAndPrint(
+                            new TimeDartLiveness(tapn, *initialMarking, query, options, strategy),
+                            options,
+                            query);
+                } else if (query->GetQuantifier() == EF || query->GetQuantifier() == AG) {
+                    WaitingList<TimeDart>* strategy = GetWaitingList<TimeDart > (query, options);
+                    VerifyAndPrint(
+                            new TimeDartReachabilitySearch(tapn, *initialMarking, query, options, strategy),
+                            options,
+                            query);
+                }
+            }
 
-	bool result = (query->GetQuantifier() == AG || query->GetQuantifier() == AF)? !verifier->Verify() : verifier->Verify();
 
-	verifier->printStats();
-	verifier->PrintTransitionStatistics();
+            delete strategy;
 
-	std::cout << "Query is " << (result ? "satisfied" : "NOT satisfied") << "." << std::endl;
-	std::cout << "Max number of tokens found in any reachable marking: ";
-	if(verifier->MaxUsedTokens() > options.GetKBound())
-		std::cout << ">" << options.GetKBound() << std::endl;
-	else
-		std::cout << verifier->MaxUsedTokens() << std::endl;
+            return 0;
+        }
 
-	if(options.GetTrace() == SOME){
-		if((query->GetQuantifier() == EF && result) || (query->GetQuantifier() == AG && !result) || (query->GetQuantifier() == EG && result) || (query->GetQuantifier() == AF && !result)) {
-			verifier->GetTrace();
-		}else {
-			std::cout << "A trace could not be generated due to the query result" << std::endl;
-		}
-	}
+        template<typename T> void VerifyAndPrint(Verification<T>* verifier, VerificationOptions& options, AST::Query* query) {
+            bool result = (query->GetQuantifier() == AG || query->GetQuantifier() == AF) ? !verifier->Verify() : verifier->Verify();
 
-	delete strategy;
+            verifier->printStats();
+            verifier->PrintTransitionStatistics();
 
-	return 0;
-}
+            std::cout << "Query is " << (result ? "satisfied" : "NOT satisfied") << "." << std::endl;
+            std::cout << "Max number of tokens found in any reachable marking: ";
+            if (verifier->MaxUsedTokens() > options.GetKBound())
+                std::cout << ">" << options.GetKBound() << std::endl;
+            else
+                std::cout << verifier->MaxUsedTokens() << std::endl;
 
-}
+            if (options.GetTrace() == SOME) {
+                if ((query->GetQuantifier() == EF && result) || (query->GetQuantifier() == AG && !result) || (query->GetQuantifier() == EG && result) || (query->GetQuantifier() == AF && !result)) {
+                    verifier->GetTrace();
+                } else {
+                    std::cout << "A trace could not be generated due to the query result" << std::endl;
+                }
+            }
+        }
+    }
 
 }
