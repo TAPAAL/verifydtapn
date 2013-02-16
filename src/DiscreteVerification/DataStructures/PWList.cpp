0188c6bd61385f444665ff413eacac4124cc2bc3
Peter Gjøl Jensen <peter.gjoel@gmail.com>
2013-02-16 18:31:51 +0100

    removed debug-code

--- src/DiscreteVerification/DataStructures/PWList.cpp
+++ src/DiscreteVerification/DataStructures/PWList.cpp
@@ -25,8 +25,6 @@ bool PWList::Add(NonStrictMarking* marking){
 	}
 	m.push_back(marking);
 	waiting_list->Add(marking, marking);
-        if(marking->passed)
-            cout << "error" << endl;
 	return true;
 }
 
