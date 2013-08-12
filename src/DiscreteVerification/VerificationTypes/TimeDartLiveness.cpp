82fc7db9ff93a5b2e1ae726a06dedb30c1f73dd4
Peter Gjoel Jensen <peter.gjoel@gmail.com>
2013-08-12 17:06:44 +0200

    removed comments

--- src/DiscreteVerification/VerificationTypes/TimeDartLiveness.cpp
+++ src/DiscreteVerification/VerificationTypes/TimeDartLiveness.cpp
@@ -135,15 +135,6 @@ namespace VerifyTAPN {
                 delete marking;
                 return false;
             }
-
-            // very curde way of doing this, integers could be passed as arguments for querychecker instead
-            // this is only needed for deadlock-checks, should possibly be contained in if-structure (huge overhead)
-            // no supported currently, gives wrong results
-            /*
-            NonStrictMarkingBase maxed = NonStrictMarkingBase(*marking);
-            maxed.incrementAge(maxDelay);        // delay to maximum possible age (for deadlock query)
-            maxed.cut();
-            */
             
             int youngest = marking->makeBase(tapn.get());
 
