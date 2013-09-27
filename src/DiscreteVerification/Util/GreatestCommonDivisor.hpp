0fb8682f0a28b03faf196036bea034b4922445cc
Jakob Taankvist <jakob_taankvist@hotmail.com>
2013-09-27 18:53:27 +0200

    Implemented the greatest common divisor functions

---
+++ src/DiscreteVerification/Util/GreatestCommonDivisor.hpp
@@ -0,0 +1,39 @@
+/*
+ * GreatestCommonDivisor.h
+ *
+ *  Created on: 27/09/2013
+ *      Author: jakob
+ */
+
+#ifndef GREATESTCOMMONDIVISOR_H_
+#define GREATESTCOMMONDIVISOR_H_
+
+#include <algorithm>
+#include <stdexcept>
+
+namespace VerifyTAPN {
+namespace DiscreteVerification {
+namespace Util {
+
+int greatestCommonDivisor(int a, int b);
+
+template <typename ForwardIterator>
+int greatestCommonDivisor(ForwardIterator first, ForwardIterator last)
+{
+	if(first == last){
+		throw new std::invalid_argument("The range is empty");
+	}
+
+	int result = *first;
+	++first;
+	while(first != last){
+		result = greatestCommonDivisor(result, *first);
+		++first;
+	}
+	return result;
+}
+
+} /* namespace Util */
+} /* namespace DiscreteVerification */
+} /* namespace VerifyTAPN */
+#endif /* GREATESTCOMMONDIVISOR_H_ */
