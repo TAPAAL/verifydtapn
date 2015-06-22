38f81b264e0c291a86b5514df1efcaecb37c79a9
Peter Gjøl Jensen <peter.gjoel@gmail.com>
2015-06-23 00:41:21 +0200

    fixed merge error

--- src/DiscreteVerification/VerificationTypes/WorkflowSoundness.cpp
+++ src/DiscreteVerification/VerificationTypes/WorkflowSoundness.cpp
@@ -130,8 +130,6 @@ bool WorkflowSoundness::addToPW(NonStrictMarking* marking, NonStrictMarking* par
         NonStrictMarking* old = pwList->addToPassed(marking, false);
 	if(old == NULL){
                 isNew = true;
-		marking->meta = new WorkflowSoundnessMetaData();
-                marking->setParent(parent);
 	} else  {
             delete marking;
             marking = old;
