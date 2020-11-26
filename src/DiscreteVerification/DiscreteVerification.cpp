3f114cc11515dc63333098fa3a3d7bfd1fc35583
pgj <root@petergjoel.dk>
2020-11-26 11:04:29 +0100

    always print strategy (even if it does not exist, implying an empty file)

--- src/DiscreteVerification/DiscreteVerification.cpp
+++ src/DiscreteVerification/DiscreteVerification.cpp
@@ -168,7 +168,7 @@ namespace VerifyTAPN { namespace DiscreteVerification {
             );
             bool result = synthesis.run();
             synthesis.print_stats();
-            if(result && !options.getStrategyFile().empty()) {
+            if(!options.getStrategyFile().empty()) {
                 if(options.getStrategyFile() == "_")
                     synthesis.write_strategy(std::cout);
                 else
