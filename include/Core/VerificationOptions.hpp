46bfaf79a3a6cbab499de34297acca5defdf777f
Tanguy Dubois <tanguy.dubois@ls2n.fr>
2025-01-22 14:03:59 +0100

    switch fix

--- include/Core/VerificationOptions.hpp
+++ include/Core/VerificationOptions.hpp
@@ -319,7 +319,7 @@ namespace VerifyTAPN {
         unsigned int cumulativeRoundingDigits = 2;
         unsigned int stepsStatsScale = 2000;
         unsigned int timeStatsScale = 2000;
-        unsigned int obsStatsScale = 100;
+        unsigned int obsStatsScale = 500;
         bool timeStdDev = false;
         unsigned int smcTraces = 0;
         SMCTracesType smcTracesType = ANY_TRACE;
